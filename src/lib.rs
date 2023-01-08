383c0a3c3bc13e99f74c5200ae88cde7f34de13f
Akash Garg <git@akashgarg.org>
2023-01-08 17:46:15 -0500

    antialiasing

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use js_sys::Math::sqrt;
 use nalgebra::{Point3, Vector3, Vector4, UnitVector3};
+use rand::Rng;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
@@ -77,6 +78,40 @@ impl Hittable for Sphere {
         return Some(HitRecord{pt: r.at(root),
                               n: if front_face { n } else { -n },
                               t: root})
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Camera {
+    origin: Point,
+    lower_left_corner: Point,
+    horizontal: Vec3,
+    vertical:Vec3
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let aspect: f32 = 16.0 / 9.0;
+
+        let viewport_height: f32 = 2.0;
+        let viewport_width: f32 = aspect * viewport_height;
+        let focal_length: f32 = 1.0;
+
+        let origin: Point = Point3::new(0_f32,0_f32,0_f32);
+        let horizontal: Vec3 = Vector3::new(viewport_width, 0_f32, 0_f32);
+        let vertical: Vec3 = Vector3::new(0_f32, viewport_height, 0_f32);
+        let lower_left_corner: Point = origin - horizontal*0.5_f32 - vertical*0.5_f32 - Vector3::new(0_f32, 0_f32, focal_length);
+
+        Camera{origin: origin,
+               lower_left_corner: lower_left_corner,
+               horizontal: horizontal,
+               vertical: vertical}
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        Ray{origin: self.origin,
+            dir: UnitVector3::new_normalize(self.lower_left_corner +
+                                            u*self.horizontal + v*self.vertical - self.origin)}
     }
 }
 
@@ -185,6 +220,7 @@ pub fn draw(
 fn render(width: u32, height: u32) -> Vec<u8>{
 
     let aspect: f32 = 16.0 / 9.0;
+    let samples_per_pixel = 10;
 
     // world
     let mut world = Vec::new();
@@ -192,14 +228,7 @@ fn render(width: u32, height: u32) -> Vec<u8>{
     world.push(Sphere{center: Point::new(0.0, -100.5, -1.0), radius: 100.0});
 
     // camera
-    let viewport_height: f32 = 2.0;
-    let viewport_width: f32 = aspect * viewport_height;
-    let focal_length: f32 = 1.0;
-
-    let origin: Point = Point3::new(0_f32,0_f32,0_f32);
-    let horizontal: Vec3 = Vector3::new(viewport_width, 0_f32, 0_f32);
-    let vertical: Vec3 = Vector3::new(0_f32, viewport_height, 0_f32);
-    let lower_left_corner: Point = origin - horizontal*0.5_f32 - vertical*0.5_f32 - Vector3::new(0_f32, 0_f32, focal_length);
+    let camera = Camera::new();
 
     // render
 
@@ -207,12 +236,29 @@ fn render(width: u32, height: u32) -> Vec<u8>{
     // image data goes from top to bottom
     for j in (0..height).rev() {
         for i in 0..width {
-            let u = i as f32 / (width-1) as f32;
-            let v = j as f32 / (height-1) as f32;
 
-            let ray = Ray{origin: origin,
-                          dir: UnitVector3::new_normalize(lower_left_corner + u*horizontal + v*vertical - origin)};
-            let pixel = ray_color(&ray, &world);
+            let mut pixel_color: Vec3 = Vec3::new(0.0,0.0,0.0);
+
+            let mut rng = rand::thread_rng();
+
+            for _ in 0..samples_per_pixel {
+                let u = (i as f32 + rng.gen::<f32>()) / (width-1) as f32;
+                let v = (j as f32 + rng.gen::<f32>()) / (height-1) as f32;
+                let ray = camera.get_ray(u, v);
+                let color = ray_color(&ray, &world);
+                pixel_color += Vec3::new(color.x as f32 / 255_f32,
+                                         color.y as f32 / 255_f32,
+                                         color.z as f32 / 255_f32);
+            }
+
+            pixel_color /= samples_per_pixel as f32;
+
+            // gamma correct
+
+            let pixel = Color::new((pixel_color.x * 255_f32).round() as u8,
+                                   (pixel_color.y * 255_f32).round() as u8,
+                                   (pixel_color.z * 255_f32).round() as u8,
+                                   255);
 
             // let r = i as f64 / (width-1) as f64;
             // let g = j as f64 / (height-1) as f64;
