1afee4e40f293493ea2168b098a8b3dacde35533
Akash Garg <git@akashgarg.org>
2023-01-12 10:27:43 -0500

    materials

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all:
 	wasm-pack build --target web
+debug:
+	wasm-pack build --debug --target web
 
 run:
 	python3 -m http.server
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 mod utils;
 
 use std::f64;
+use std::fmt;
+use std::fmt::Debug;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use js_sys::Math::sqrt;
+use js_sys::Math::pow;
 use nalgebra::{Point3, Vector3, Vector4, UnitVector3};
 use rand::Rng;
+use rand::rngs::ThreadRng;
+use almost;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
@@ -27,11 +32,13 @@ pub struct Ray {
     dir: UnitVec3
 }
 
-#[derive(Copy,Clone,Debug)]
+#[derive(Clone,Debug)]
 pub struct HitRecord {
     pt: Point,
     n: UnitVec3,
-    t: f32
+    t: f32,
+    front_face: bool,
+    mat: Box<dyn Material>
 }
 
 pub trait Hittable {
@@ -44,9 +51,126 @@ impl Ray {
     }
 }
 
+// scatters an incoming ray and outputs the color attenuation and the scattered
+// ray.
+pub trait Material: MaterialClone {
+    fn scatter(&self, rng: &mut ThreadRng, ray: &Ray, rec: &HitRecord) -> Option<(Vec3, Ray)>;
+}
+
+pub trait MaterialClone {
+    fn clone_box(&self) -> Box<dyn Material>;
+}
+
+impl<T> MaterialClone for T
+where
+    T: 'static + Material + Clone + Debug
+{
+    fn clone_box(&self) -> Box<dyn Material> {
+        Box::new(self.clone())
+    }
+}
+
+// We can now implement Clone manually by forwarding to clone_box.
+impl Clone for Box<dyn Material> {
+    fn clone(&self) -> Box<dyn Material> {
+        self.clone_box()
+    }
+}
+
+impl Debug for Box<dyn Material> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // TODO: what should go here?
+        //(*self).fmt(f)
+        Ok(())
+    }
+}
+
+#[derive(Clone,Debug)]
+struct Lambertian {
+    albedo: Vec3
+}
+
+#[derive(Clone,Debug)]
+struct Metal {
+    albedo: Vec3,
+    fuzz: f32
+}
+
+#[derive(Clone,Debug)]
+struct Dielectric {
+    ir: f32 // index of refraction
+}
+
+impl Dielectric {
+    fn reflectance(cosine: f32, ref_idx: f32) -> f32 {
+        // Use Schlick's approximation for reflectance.
+        let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+        r0 = r0*r0;
+        return r0 + (1.0 - r0)*pow(1.0 - cosine as f64, 5.0) as f32;
+    }
+}
+
+impl Material for Lambertian {
+    fn scatter(&self, rng: &mut ThreadRng, _ray: &Ray, rec: &HitRecord) -> Option<(Vec3, Ray)> {
+        let attenuation = self.albedo;
+        let scatter_direction = rec.n.into_inner() + random_in_hemisphere(rng, &rec.n).into_inner();
+
+        if scatter_direction.iter().any(|&v| almost::zero(v)) {
+            return Some((attenuation, Ray{origin: rec.pt, dir: rec.n}));
+        } else {
+            let ray = Ray{origin: rec.pt,
+                          dir: UnitVec3::new_normalize(scatter_direction)};
+            return Some((attenuation, ray));
+        }
+    }
+}
+
+impl Material for Metal {
+    fn scatter(&self, rng: &mut ThreadRng, ray: &Ray, rec: &HitRecord) -> Option<(Vec3, Ray)> {
+        let attenuation = self.albedo;
+        let reflected = ray.dir.into_inner() - 2.0*ray.dir.dot(&rec.n)*rec.n.into_inner();
+        let scattered = UnitVec3::new_normalize(reflected + self.fuzz * random_in_unit_sphere(rng).into_inner());
+        if scattered.dot(&rec.n) > 0.0 {
+            let scattered_ray = Ray{origin: rec.pt, dir: scattered};
+            return Some((attenuation, scattered_ray));
+        } else {
+            return None
+        }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, rng: &mut ThreadRng, ray: &Ray, rec: &HitRecord) -> Option<(Vec3, Ray)> {
+        let attenuation = Vec3::new(1.0,1.0,1.0);
+        let refraction_ratio = if rec.front_face { 1.0/self.ir } else { self.ir };
+
+        // refract the ray
+        let uv = ray.dir.into_inner();
+        let n = rec.n.into_inner();
+        let cos_theta = (-ray.dir.dot(&rec.n)).min(1.0);
+
+        let sin_theta = sqrt((1.0 - cos_theta*cos_theta) as f64) as f32;
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction;
+        if cannot_refract || Dielectric::reflectance(cos_theta, refraction_ratio) > rng.gen::<f32>() {
+            direction = uv - 2.0*uv.dot(&n)*n;
+        } else {
+            let r_out_perp = refraction_ratio * (uv + cos_theta * n);
+            let r_out_parallel = -sqrt((1.0 - r_out_perp.dot(&r_out_perp)).abs() as f64) as f32 * n;
+            direction = r_out_perp + r_out_parallel;
+        }
+
+        return Some((attenuation,
+                     Ray{origin: rec.pt,
+                         dir: UnitVec3::new_normalize(direction)}));
+    }
+}
+
 struct Sphere {
     center: Point,
-    radius: f32
+    radius: f32,
+    mat: Box<dyn Material>
 }
 
 impl Hittable for Sphere {
@@ -64,20 +188,25 @@ impl Hittable for Sphere {
         let sqrtd = sqrt(discriminant as f64) as f32;
 
         // Find the nearest root that lies in the acceptable range.
-        let root = (-half_b - sqrtd) / a;
+        let mut root = (-half_b - sqrtd) / a;
         if root < t_min || t_max < root {
-            let root2 = (-half_b + sqrtd) / a;
-            if root2 < t_min || t_max < root2 {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
                 return None
             }
         }
 
-        let n = UnitVector3::new_normalize(r.at(root) - self.center);
-        let front_face = r.dir.dot(&n) < 0_f32;
+        // we divide by radius here even though we are normalizing since a
+        // negative radius will invert the normals, which might be something we
+        // want to glass spheres.
+        let n = UnitVector3::new_normalize((r.at(root) - self.center) / self.radius);
+        let front_face = r.dir.dot(&n) <= 0_f32;
 
         return Some(HitRecord{pt: r.at(root),
                               n: if front_face { n } else { -n },
-                              t: root})
+                              t: root,
+                              front_face: front_face,
+                              mat: self.mat.clone()})
     }
 }
 
@@ -115,6 +244,24 @@ impl Camera {
     }
 }
 
+fn random_in_unit_sphere(rng: &mut ThreadRng) -> UnitVec3 {
+    let p = Vec3::new(rng.gen_range(-1.0..1.0),
+                      rng.gen_range(-1.0..1.0),
+                      rng.gen_range(-1.0..1.0));
+    return UnitVec3::new_normalize(p);
+}
+
+fn random_in_hemisphere(rng: &mut ThreadRng, normal: &Vec3) -> UnitVec3 {
+    let in_unit_sphere = random_in_unit_sphere(rng);
+    if in_unit_sphere.dot(&normal) > 0.0 {
+        // In the same hemisphere as the normal
+        return in_unit_sphere;
+    }
+    else {
+        return -in_unit_sphere;
+    }
+}
+
 fn hit_sphere(center: &Point, radius: f32, r: &Ray) -> f32 {
     let oc = r.origin - center;
     let a = r.dir.dot(&r.dir);
@@ -128,33 +275,61 @@ fn hit_sphere(center: &Point, radius: f32, r: &Ray) -> f32 {
     }
 }
 
-fn ray_color<T: Hittable>(ray: &Ray, world: &Vec<T>) -> Color {
-    for item in world {
-        match item.hit(&ray, 0_f32, f32::MAX) {
-            None => continue,
-            Some(rec) => {
-                if rec.t > 0.0 {
-                    let color = 0.5 * Vector3::new(rec.n.x + 1.0,
-                                                   rec.n.y + 1.0,
-                                                   rec.n.z + 1.0);
-                    return Color::new((255_f32 * color.x).round() as u8,
-                                      (255_f32 * color.y).round() as u8,
-                                      (255_f32 * color.z).round() as u8,
-                                      255);
-                }
-            }
-        }
+fn ray_color<T: Hittable>(ray: &Ray, world: &Vec<T>, rng: &mut ThreadRng, depth: u32) -> Vec3 {
+
+    // exceeded ray bounce limit, no more light is gathered
+    if depth <= 0 {
+        return Vec3::new(0.0,0.0,0.0);
     }
+
+    // get the closest hit pt from the ray to world
+    let closest_hit = world.iter().map(|item| item.hit(&ray, 0.001, f32::MAX))
+                                  .filter(|val| val.is_some())
+                                  .min_by(|a,b| {
+                                      let at: f32 = a.as_ref().unwrap().t;
+                                      let bt: f32 = b.as_ref().unwrap().t;
+                                      at.total_cmp(&bt)
+                                  });
+    match closest_hit {
+        // if there is a closest hit
+        Some(hit) => {
+            match hit {
+                // get the hit record if there is one
+                Some(rec) =>  {
+                    let color = match rec.mat.scatter(rng, &ray, &rec) {
+                        Some((attenuation, scattered_ray)) => {
+                            attenuation.component_mul(&ray_color(&scattered_ray, world, rng, depth - 1))
+                        },
+                        None => Vec3::new(0.0,0.0,0.0)
+                    };
+                    return color;
+                },
+                _ => { return Vec3::new(0.0,0.0,0.0) }
+            }
+        },
+        _ => {}
+    }
+
+    // for item in world {
+    //     match item.hit(&ray, 0.001, f32::MAX) {
+    //         None => continue,
+    //         Some(rec) => {
+    //             if rec.t > 0.0 {
+    //                 let color = match rec.mat.scatter(rng, &ray, &rec) {
+    //                     Some((attenuation, scattered_ray)) => {
+    //                         attenuation.component_mul(&ray_color(&scattered_ray, world, rng, depth - 1))
+    //                     },
+    //                     None => Vec3::new(0.0,0.0,0.0)
+    //                 };
+    //                 return color;
+    //             }
+    //         }
+    //     }
+    // }
+
     // background
     let t = 0.5*(ray.dir.y + 1.0);
-    Color::new((255_f32 * (1.0-t)).round() as u8,
-               (255_f32 * (1.0-t)).round() as u8,
-               (255_f32 * (1.0-t)).round() as u8,
-               255) +
-        Color::new((255_f32 * 0.5 * t).round() as u8,
-                   (255_f32 * 0.7 * t).round() as u8,
-                   (255_f32 * t).round() as u8,
-                   255)
+    Vec3::new(1.0-t, 1.0-t, 1.0-t) + Vec3::new(0.5 * t, 0.7 * t, t)
 }
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -219,13 +394,25 @@ pub fn draw(
 
 fn render(width: u32, height: u32) -> Vec<u8>{
 
-    let aspect: f32 = 16.0 / 9.0;
     let samples_per_pixel = 10;
+    let max_depth = 10;
 
     // world
     let mut world = Vec::new();
-    world.push(Sphere{center: Point::new(0.0, 0.0, -1.0), radius: 0.5});
-    world.push(Sphere{center: Point::new(0.0, -100.5, -1.0), radius: 100.0});
+
+    // materials
+    let material_ground = Box::new(Lambertian{albedo: Vec3::new(0.8, 0.8, 0.0)});
+    //let material_center = Box::new(Lambertian{albedo: Vec3::new(0.7, 0.3, 0.3)});
+    let material_center = Box::new(Lambertian{albedo: Vec3::new(0.1, 0.2, 0.5)});
+    // let material_left = Box::new(Metal{albedo: Vec3::new(0.8, 0.8, 0.8), fuzz: 0.3});
+    let material_left = Box::new(Dielectric{ir: 1.5});
+    let material_right = Box::new(Metal{albedo: Vec3::new(0.8, 0.6, 0.2), fuzz: 0.0});
+
+    world.push(Sphere{center: Point::new(0.0, -100.5, -1.0), radius: 100.0, mat: material_ground});
+    world.push(Sphere{center: Point::new(0.0, 0.0, -1.0), radius: 0.5, mat: material_center});
+    world.push(Sphere{center: Point::new(-1.0, 0.0, -1.0), radius: 0.5, mat: material_left.clone()});
+    world.push(Sphere{center: Point::new(-1.0, 0.0, -1.0), radius: -0.4, mat: material_left.clone()});
+    world.push(Sphere{center: Point::new(1.0, 0.0, -1.0), radius: 0.5, mat: material_right});
 
     // camera
     let camera = Camera::new();
@@ -244,16 +431,18 @@ fn render(width: u32, height: u32) -> Vec<u8>{
             for _ in 0..samples_per_pixel {
                 let u = (i as f32 + rng.gen::<f32>()) / (width-1) as f32;
                 let v = (j as f32 + rng.gen::<f32>()) / (height-1) as f32;
+                // let u = (i as f32) / (width-1) as f32;
+                // let v = (j as f32) / (height-1) as f32;
                 let ray = camera.get_ray(u, v);
-                let color = ray_color(&ray, &world);
-                pixel_color += Vec3::new(color.x as f32 / 255_f32,
-                                         color.y as f32 / 255_f32,
-                                         color.z as f32 / 255_f32);
+                pixel_color += ray_color(&ray, &world, &mut rng, max_depth);
             }
 
             pixel_color /= samples_per_pixel as f32;
 
-            // gamma correct
+            // gamma correct for gamma = 2
+            pixel_color.x = sqrt(pixel_color.x as f64) as f32;
+            pixel_color.y = sqrt(pixel_color.y as f64) as f32;
+            pixel_color.z = sqrt(pixel_color.z as f64) as f32;
 
             let pixel = Color::new((pixel_color.x * 255_f32).round() as u8,
                                    (pixel_color.y * 255_f32).round() as u8,
